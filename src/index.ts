03d2c78beac3d98fa47f36cb913c999cf251bb83
NOAH HAMBAYI <noah@hambayi.com>
2016-10-08 19:13:38 -0700

    configured linter

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import PackageBuilder from "./PackageBuilder";
 import ArgumentsParser from "./ArgumentsParser";
 
-let myArgs = process.argv.slice(2);
  console.log("myArgs: ", process);
 
-process.argv.forEach(function (val, index, array) {
-            console.log(index + " : " + val);
-        });
+
 
